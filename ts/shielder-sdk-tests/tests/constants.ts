37eb75d72cfbced4da934ebb8b0169727e4e144f
Maksym Zub <kroistsmeh@gmail.com>
2025-08-08 09:14:47 +0200

    Upd

--- ts/shielder-sdk-tests/tests/constants.ts
+++ ts/shielder-sdk-tests/tests/constants.ts
@@ -1,3 +1,3 @@
-export const INITIAL_EVM_BALANCE = 1_000_000_000_000_000n;
+export const INITIAL_EVM_BALANCE_GAS_POINTS = 1_000_000_000_000_000n;
 
 export const ACCOUNT_NAMES = ["alice", "bob", "charlie", "dave"] as const;
